f686d1e1c6407d66571e3e500b859f74f6a21db5
SelfMadeSystem <sms@shoghisimon.cc>
2023-08-10 20:58:41 -0400

    removed scroll fade kuz that's actually causing more problems. also thinking I need to seperate stuff to ease stuff

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
 import 'virtual:uno.css';
 import "./components";
-import './scroll-fade';
 import './theme';
--- uno.config.ts
+++ uno.config.ts
@@ -1,16 +1,3 @@
 import { defineConfig } from 'unocss'
 
-export default defineConfig({
-  variants: [
-    // scroll-visible:
-    (matcher) => {
-        if (!matcher.startsWith('scroll-visible:'))
-          return matcher
-        return {
-          // slice `scroll-visible:` prefix and passed to the next variants and rules
-          matcher: matcher.slice(15),
-          selector: s => `${s}.scroll-fade--visible`,
-        }
-      } 
-  ]
-})
+export default defineConfig({})
