976ec1435e442ae640b0af00379d851b490aff42
SelfMadeSystem <sms@shoghisimon.cc>
2023-08-23 13:04:39 -0400

    Added tooltip to buttons

--- src/sections/SectionThree.ts
+++ src/sections/SectionThree.ts
@@ -29,19 +29,23 @@ export class SectionThree extends LitElement {
                         <div class="flex-1 grow-1 flex flex-col justify-between">
                             <portfolio-card class="portfolio-card flex flex-col-reverse justify-between gap-xl p-8 rounded-8">
                                 <div class="flex flex-row items-center justify-center">
-                                    <button data-for="about" class="active">
+                                    <button data-for="about" class="active" aria-label="about">
                                         <svg xmlns="http://www.w3.org/2000/svg"
                                         viewBox="0 0 24 24">
                                         <path
                                             d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z" />
                                         </svg>
+
+                                        <span class="tooltip">About</span>
                                     </button>
-                                    <button data-for="technologies">
+                                    <button data-for="technologies" aria-label="technologies">
                                         <svg xmlns="http://www.w3.org/2000/svg"
                                         viewBox="0 0 24 24">
                                         <path
                                             d="M21.71 20.29L20.29 21.71A1 1 0 0 1 18.88 21.71L7 9.85A3.81 3.81 0 0 1 6 10A4 4 0 0 1 2.22 4.7L4.76 7.24L5.29 6.71L6.71 5.29L7.24 4.76L4.7 2.22A4 4 0 0 1 10 6A3.81 3.81 0 0 1 9.85 7L21.71 18.88A1 1 0 0 1 21.71 20.29M2.29 18.88A1 1 0 0 0 2.29 20.29L3.71 21.71A1 1 0 0 0 5.12 21.71L10.59 16.25L7.76 13.42M20 2L16 4V6L13.83 8.17L15.83 10.17L18 8H20L22 4Z" />
                                         </svg>
+
+                                        <span class="tooltip">Technologies</span>
                                     </button>
                                 </div>
                                 <div class="flex flex-col justify-between gap-4">
@@ -82,19 +86,23 @@ export class SectionThree extends LitElement {
                         <div class="flex-1 grow-1 flex flex-col justify-between">
                             <portfolio-card class="portfolio-card flex flex-col-reverse justify-between gap-xl p-8 rounded-8">
                                 <div class="flex flex-row items-center justify-center">
-                                    <button data-for="about" class="active">
+                                    <button data-for="about" class="active" aria-label="about">
                                         <svg xmlns="http://www.w3.org/2000/svg"
                                         viewBox="0 0 24 24">
                                         <path
                                             d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z" />
                                         </svg>
+
+                                        <span class="tooltip">About</span>
                                     </button>
-                                    <button data-for="technologies">
+                                    <button data-for="technologies" aria-label="technologies">
                                         <svg xmlns="http://www.w3.org/2000/svg"
                                         viewBox="0 0 24 24">
                                         <path
                                             d="M21.71 20.29L20.29 21.71A1 1 0 0 1 18.88 21.71L7 9.85A3.81 3.81 0 0 1 6 10A4 4 0 0 1 2.22 4.7L4.76 7.24L5.29 6.71L6.71 5.29L7.24 4.76L4.7 2.22A4 4 0 0 1 10 6A3.81 3.81 0 0 1 9.85 7L21.71 18.88A1 1 0 0 1 21.71 20.29M2.29 18.88A1 1 0 0 0 2.29 20.29L3.71 21.71A1 1 0 0 0 5.12 21.71L10.59 16.25L7.76 13.42M20 2L16 4V6L13.83 8.17L15.83 10.17L18 8H20L22 4Z" />
                                         </svg>
+
+                                        <span class="tooltip">Technologies</span>
                                     </button>
                                 </div>
                                 <div class="flex flex-col justify-between gap-4">
