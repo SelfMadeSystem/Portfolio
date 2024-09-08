da3422bf01a9083b3ba53c5f64d1b2d47a556ab1
SelfMadeSystem <sms@shoghisimon.cc>
2024-09-08 18:13:30 -0400

    Improved drastically the titles of each section

--- src/sections/SectionFive.ts
+++ src/sections/SectionFive.ts
@@ -11,12 +11,74 @@ export class SectionFive extends LitElement {
         // @unocss-include
         return html`
             <portfolio-section class="primary z-1" id="contact">
-                <div id="game-development" class="primary-card z-2 my-24 flex flex-col items-center p-8 rounded-8">
-                    <h2 class="text-4xl">Game Development</h2>
-                    <p class="text-xl max-w-lg text-center mx-8">
-                        I am experienced in game development using Unity, Godot and C# as well as web-based game
-                        development using TypeScript.
-                    </p>
+                <div id="game-development" class="primary-card z-2 mb-24 flex flex-col items-center p-8 rounded-8">
+                    <portfolio-card class="portfolio-card flex flex-col-reverse justify-between gap-xl">
+                        <div class="flex flex-row items-center justify-center">
+                            <button data-for="about" class="active" aria-label="about">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <path
+                                        d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z"
+                                    />
+                                </svg>
+
+                                <span class="tooltip">About</span>
+                            </button>
+                            <button data-for="technologies" aria-label="technologies">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <path
+                                        d="M21.71 20.29L20.29 21.71A1 1 0 0 1 18.88 21.71L7 9.85A3.81 3.81 0 0 1 6 10A4 4 0 0 1 2.22 4.7L4.76 7.24L5.29 6.71L6.71 5.29L7.24 4.76L4.7 2.22A4 4 0 0 1 10 6A3.81 3.81 0 0 1 9.85 7L21.71 18.88A1 1 0 0 1 21.71 20.29M2.29 18.88A1 1 0 0 0 2.29 20.29L3.71 21.71A1 1 0 0 0 5.12 21.71L10.59 16.25L7.76 13.42M20 2L16 4V6L13.83 8.17L15.83 10.17L18 8H20L22 4Z"
+                                    />
+                                </svg>
+
+                                <span class="tooltip">Technologies</span>
+                            </button>
+                        </div>
+                        <div class="flex flex-col justify-between gap-4 w-max">
+                            <h2 class="text-4xl px-12">Game Development</h2>
+                            <multi-height class="relative w-full">
+                                <div
+                                    data-name="about"
+                                    class="content active text-xl text-center m-auto absolute top-0 left-0 w-full"
+                                >
+                                    I am experienced in game development using Unity, Godot and C# as well as web-based
+                                    game development using TypeScript. . Click on
+                                    <span class="inline-flex items-center gap-1">
+                                        <svg class="smol-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                            <path
+                                                d="M21.71 20.29L20.29 21.71A1 1 0 0 1 18.88 21.71L7 9.85A3.81 3.81 0 0 1 6 10A4 4 0 0 1 2.22 4.7L4.76 7.24L5.29 6.71L6.71 5.29L7.24 4.76L4.7 2.22A4 4 0 0 1 10 6A3.81 3.81 0 0 1 9.85 7L21.71 18.88A1 1 0 0 1 21.71 20.29M2.29 18.88A1 1 0 0 0 2.29 20.29L3.71 21.71A1 1 0 0 0 5.12 21.71L10.59 16.25L7.76 13.42M20 2L16 4V6L13.83 8.17L15.83 10.17L18 8H20L22 4Z"
+                                            />
+                                        </svg>
+                                        "Technologies"
+                                    </span>
+                                    for more information.
+                                </div>
+                                <div
+                                    data-name="technologies"
+                                    class="content text-xl text-center m-auto absolute top-0 left-0 w-full"
+                                >
+                                    <h3 class="text-3xl my-0">Technologies</h3>
+                                    <div class="flex flex-row justify-center gap-4">
+                                        <div class="flex flex-col items-center">
+                                            <h4 class="text-2xl my-0">Languages</h4>
+                                            <div>JavaScript</div>
+                                            <div>TypeScript</div>
+                                            <div>C#</div>
+                                            <div>GDScript</div>
+                                            <div>Java</div>
+                                            <div>Rust</div>
+                                        </div>
+                                        <div class="flex flex-col items-center">
+                                            <h4 class="text-2xl my-0">Game Engines</h4>
+                                            <div>Godot</div>
+                                            <div>Unity</div>
+                                            <div>LWJGL</div>
+                                            <div>HTML5</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </multi-height>
+                        </div>
+                    </portfolio-card>
                 </div>
 
                 <div class="pb-16 md:pl-15% w-full z-2">
